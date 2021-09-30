35d1b6f8bd71bd91bf9b64decf0c8ac063124a9f
pedroluiznogueira <peluiznogueiram@hotmail.com>
2021-09-30 20:10:25 -0300

    Iniciando o JFrame na classe Loja

---
+++ out/production/Loja/bda_lojinha/lojinha.sql
@@ -0,0 +1,7 @@
+create schema lojinha;
+
+create table produtos (
+    id int primary key auto_increment
+);
+
+select * from produtos;
--- src/Loja.java
+++ src/Loja.java
@@ -1,8 +1,23 @@
 import javax.swing.*;
 
 public class Loja {
+    // referenciando o JPanel
     private JPanel Main;
-    private JTextField textField1;
+    // inputs
+    private JTextField produtoInput;
+    private JTextField nomeInput;
+    private JTextField precoInput;
+    private JTextField quantidadeInput;
+    // botoes
     private JButton salvarButton;
     private JButton deletarButton;
+
+    public static void main(String[] args) {
+        // instanciando o JFrame
+        JFrame frame = new JFrame("Loja");
+        frame.setContentPane(new Loja().Main);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
 }
