0cf3a93dedafef94cbae182d2d5a5bbd3027ed16
pedroluiznogueira <peluiznogueiram@hotmail.com>
2021-09-30 23:01:05 -0300

    Para cada produto da entidade produtos do banco de dados, terei um objeto Produto da classe Produto, com seus atributos sendo cada campo da entidade do banco

--- src/entities/Produto.java
+++ src/entities/Produto.java
@@ -1,4 +1,34 @@
 package entities;
 
 public class Produto {
+    private String nome;
+    private String preco;
+    private String quantidade;
+
+    public Produto() {
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getPreco() {
+        return preco;
+    }
+
+    public void setPreco(String preco) {
+        this.preco = preco;
+    }
+
+    public String getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(String quantidade) {
+        this.quantidade = quantidade;
+    }
 }
