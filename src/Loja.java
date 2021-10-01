f0cb3e9e86bda196ed040d0a42ee3196616a039d
pedroluiznogueira <peluiznogueiram@hotmail.com>
2021-09-30 21:01:54 -0300

    Realizando conexão com o banco de dados

--- src/Loja.java
+++ src/Loja.java
@@ -1,6 +1,7 @@
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.sql.*;
 
 public class Loja {
     // referenciando o JPanel
@@ -14,6 +15,11 @@ public class Loja {
     private JButton salvarButton;
     private JButton deletarButton;
 
+    // conexao com banco
+    Connection conexao;
+    // variavel para tratar comandos sql
+    PreparedStatement pst;
+
     public static void main(String[] args) {
         // instanciando o JFrame
         JFrame frame = new JFrame("Loja");
@@ -24,17 +30,38 @@ public class Loja {
     }
 
     public Loja() {
+        // realizando a conexão com o banco
+        ConexaoBanco();
+
+        // addActionListener implementa actionPerformed e vai executar seu escopo quando o botão for clicado
         salvarButton.addActionListener(new ActionListener() {
 
             @Override
             public void actionPerformed(ActionEvent e) {
+                // recebendo os input do usuário
                 String nome = nomeInput.getText();
                 String produto = produtoInput.getText();
                 String preco = precoInput.getText();
                 String quantidade = quantidadeInput.getText();
 
-                System.out.println(nome);
+
             }
         });
+    }
+
+    public void ConexaoBanco() {
+        // tratando as excessões necessárias
+        try {
+            // relacionando o driver, 8.0.22 precisou explicitar a timezone
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            // essa conexão fará a ponte para que sejam executados os comandos sql nessa variável
+            conexao = DriverManager.getConnection("jdbc:mysql://localhost:3306/lojinha", "root", "root");
+            System.out.println("A conexão foi bem sucedida");
+
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
